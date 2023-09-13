872d01edec5e1c19b29b98369b37dda43fd60905
Sheldon <aneln700@gmail.com>
2023-09-13 19:48:11 +0300

    Initial

--- src/test/java/ru/netology/CashBackHackerTest.java
+++ src/test/java/ru/netology/CashBackHackerTest.java
@@ -1,8 +1,39 @@
 package ru.netology;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.*;
 
-class CashBackHackerTest {
-
+public class CashBackHackerTest {
+    @org.testng.annotations.Test
+    public void testRemain() {
+        CashBackHacker service = new CashBackHacker();
+        int amount = 500;
+        int actual = service.remain(amount);
+        int expected = 500;
+        org.testng.Assert.assertEquals(actual, expected);
+    }
+    @org.testng.annotations.Test
+    public void testAmountEqual() {
+        CashBackHacker service = new CashBackHacker();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        org.testng.Assert.assertEquals(actual, expected);
+    }
+    @org.testng.annotations.Test
+    public void testAmountLess() {
+        CashBackHacker service = new CashBackHacker();
+        int amount = 999;
+        int actual = service.remain(amount);
+        int expected = 1;
+        org.testng.Assert.assertEquals(actual, expected);
+    }
+    @org.testng.annotations.Test
+    public void testAmountMore() {
+        CashBackHacker service = new CashBackHacker();
+        int amount = 1001;
+        int actual = service.remain(amount);
+        int expected = 0;
+        org.testng.Assert.assertEquals(actual, expected);
+    }
 
 }
