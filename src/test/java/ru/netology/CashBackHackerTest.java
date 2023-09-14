6b0c79d8b319a17d44f9f09fb0671835f3797df3
Sheldon <aneln700@gmail.com>
2023-09-14 20:28:46 +0300

    Initial2

--- src/test/java/ru/netology/CashBackHackerTest.java
+++ src/test/java/ru/netology/CashBackHackerTest.java
@@ -3,37 +3,37 @@ package ru.netology;
 import static org.testng.Assert.*;
 
 public class CashBackHackerTest {
-    @org.testng.annotations.Test
+    @org.junit.jupiter.api.Test
     public void testRemain() {
         CashBackHacker service = new CashBackHacker();
         int amount = 500;
         int actual = service.remain(amount);
         int expected = 500;
-        org.testng.Assert.assertEquals(actual, expected);
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
     }
-    @org.testng.annotations.Test
+    @org.junit.jupiter.api.Test
     public void testAmountEqual() {
         CashBackHacker service = new CashBackHacker();
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        org.testng.Assert.assertEquals(actual, expected);
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
     }
-    @org.testng.annotations.Test
+    @org.junit.jupiter.api.Test
     public void testAmountLess() {
         CashBackHacker service = new CashBackHacker();
         int amount = 999;
         int actual = service.remain(amount);
         int expected = 1;
-        org.testng.Assert.assertEquals(actual, expected);
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
     }
-    @org.testng.annotations.Test
+    @org.junit.jupiter.api.Test
     public void testAmountMore() {
         CashBackHacker service = new CashBackHacker();
         int amount = 1001;
         int actual = service.remain(amount);
         int expected = 0;
-        org.testng.Assert.assertEquals(actual, expected);
+        org.junit.jupiter.api.Assertions.assertEquals(expected, actual);
     }
 
 }
